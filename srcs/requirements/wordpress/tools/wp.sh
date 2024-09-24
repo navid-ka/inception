12656aaed77ba450688abb6b294a6b1992b890cf
Navid Keyani Arana <nkeyani-@cbr7s6.42barcelona.com>
2024-09-24 18:31:23 +0200

    new wordpress script

--- srcs/requirements/wordpress/tools/wp.sh
+++ srcs/requirements/wordpress/tools/wp.sh
@@ -1,11 +1,38 @@
-#! /bin/bash
+#!/bin/sh
 
-if [ ! -f ./wp-config.php ]; then
-	wp core download --allow-root
-	wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$MYSQL_HOSTNAME --allow-root
-	wp core install --url=$DONAIN_NAME --title="$WORDPRESS_TITLE" --admin_user=$WORDPRESS_ADMIM --admin_password=$WORDPRESS_ADMIM_PASS --admin_email=$WORDPRESS_ADMIM_EMAIL --skip-email --allow-root
-	wp user create $WORDPRESS_USER $WORDPRESS_EMAIL --role=author --user_pass=$WORDPRESS_USER_PASS --allow-root
-	wp theme install twentytwentytwo --activate --allow-root
+mkdir -p /var/www/html
+
+if [ -f /var/www/html/wp-config.php ]; then
+  echo "wordpress already downloaded"
+else
+  echo "Downloading WordPress..."
+
+  wget http://wordpress.org/latest.tar.gz || {
+    echo "Failed to download WordPress"
+    exit 1
+  }
+  tar xfz latest.tar.gz || {
+    echo "Failed to extract WordPress"
+    exit 1
+  }
+  mv wordpress/* /var/www/html/ || {
+    echo "Failed to move WordPress files"
+    exit 1
+  }
+  rm -rf latest.tar.gz
+  rm -rf wordpress
+
+  sed -i "s/username_here/$MYSQL_USER/g" /var/www/html/wp-config-sample.php
+  sed -i "s/password_here/$MYSQL_PASSWORD/g" /var/www/html/wp-config-sample.php
+  sed -i "s/localhost/$MYSQL_HOSTNAME/g" /var/www/html/wp-config-sample.php
+  sed -i "s/database_name_here/$MYSQL_DATABASE/g" /var/www/html/wp-config-sample.php
+
+  cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
+
+  chown -R www-data:www-data /var/www/html
+  chmod -R 755 /var/www/html
+
+  echo "WordPress correctly downloaded"
 fi
-
-/usr/sbin/php-fpm7.4 -F
+#execute to init server
+exec "$@"
